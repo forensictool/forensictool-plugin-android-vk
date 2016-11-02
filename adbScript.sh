4ffd6060c401fae17b6f3c71c26c86666892c0c7
Anton Sapunov <fort.sav.28@gmail.com>
2016-11-03 02:45:39 +0700

    Corrected pullScript

--- adbScript.sh
+++ adbScript.sh
@@ -2,22 +2,24 @@
 
 adb root
 adbState=`adb get-state`
-appList=`adb shell pm list packages -f > appList.txt`
+if [[ "$adbState" = "device" ]]
+then
+appList=`adb shell pm list packages -f > appList.txt`  # Все установленные приложения на смартфоне
 echo "Введите искомое приложение: "
 	read neededApp
-grepInfo=`grep "$neededApp" appList.txt`
-if [[ "$adbState" = "device" ]]
+grepInfo=`grep "$neededApp" appList.txt` # Поиск нужного приложения
 if [[ "$grepInfo" != "" ]]
 then 
 	echo "Есть такое приложение!"
-else 
-	echo "Нет такого приложения!"
-fi
-then
 	echo "Введите директорию, которую необходимо запулить: "
 	read dir_name
 	mkdir Pulling
 	echo `adb pull $dir_name Pulling`
+
+else 
+	echo "Нет такого приложения!"
+	exit 1
+fi
 else
 	echo "Подключите устройство!"
 	exit 1
